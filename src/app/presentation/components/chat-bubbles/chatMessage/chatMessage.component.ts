c1a971b01f5318a44debb62488ecc8c190d67724
Luis Felipe Garcia <fellgar246@gmail.com>
2024-03-02 12:52:18 -0600

    enabled image generation

--- src/app/presentation/components/chat-bubbles/chatMessage/chatMessage.component.ts
+++ src/app/presentation/components/chat-bubbles/chatMessage/chatMessage.component.ts
@@ -14,5 +14,6 @@ export class ChatMessageComponent {
 
   @Input({ required: true }) text!: string;
   @Input() audioUrl?: string;
+  @Input() imageInfo?: { url: string, alt: string };
  
 }
--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,34 +1,42 @@
-import { Injectable } from '@angular/core';
-import { orthographyUseCase, prosConsUseCase, prosConsStreamUseCase, translateTextUseCase, textToAudioUseCase, audioToTextUseCase } from '@use-cases/index';
-import { from } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  orthographyUseCase,
+  prosConsUseCase,
+  prosConsStreamUseCase,
+  translateTextUseCase,
+  textToAudioUseCase,
+  audioToTextUseCase,
+  imageGenerationUseCase,
+} from "@use-cases/index";
+import { from } from "rxjs";
 
-
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class OpenAiService {
-
-
-  checkOrthography( prompt: string ) {
-    return from( orthographyUseCase(prompt) ) ;
+  checkOrthography(prompt: string) {
+    return from(orthographyUseCase(prompt));
   }
 
-  prosConsDiscusser( prompt: string ) {
-    return from( prosConsUseCase(prompt) );
+  prosConsDiscusser(prompt: string) {
+    return from(prosConsUseCase(prompt));
   }
 
   prosConsStreamDiscusser(prompt: string, abortSignal: AbortSignal) {
-    return prosConsStreamUseCase(prompt, abortSignal) ;
+    return prosConsStreamUseCase(prompt, abortSignal);
   }
 
-  translateText( prompt: string, lang: string){
-    return from( translateTextUseCase(prompt, lang) )
+  translateText(prompt: string, lang: string) {
+    return from(translateTextUseCase(prompt, lang));
   }
 
-  textToAudio( prompt: string, voice: string){
-    return from( textToAudioUseCase(prompt, voice) )
+  textToAudio(prompt: string, voice: string) {
+    return from(textToAudioUseCase(prompt, voice));
   }
 
-  audioToText( file: File, prompt?: string){
-    return from( audioToTextUseCase(file, prompt) )
+  audioToText(file: File, prompt?: string) {
+    return from(audioToTextUseCase(file, prompt));
+  }
+
+  imageGeneration(prompt: string, originalImage?: string, maskImage?: string) {
+    return from(imageGenerationUseCase(prompt, originalImage, maskImage));
   }
 }
