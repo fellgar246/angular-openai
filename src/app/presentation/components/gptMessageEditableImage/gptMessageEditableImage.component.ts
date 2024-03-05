444b32cbf62dc7e962932a4380ff481117dbc4cc
Luis Felipe Garcia <fellgar246@gmail.com>
2024-03-04 18:42:18 -0600

    add mouse drawing

--- src/app/presentation/components/gptMessageEditableImage/gptMessageEditableImage.component.ts
+++ src/app/presentation/components/gptMessageEditableImage/gptMessageEditableImage.component.ts
@@ -19,6 +19,8 @@ export class GptMessageEditableImageComponent implements AfterViewInit {
     @Output() onSelectedImage = new EventEmitter<string>();
 
     public originalImage = signal<HTMLImageElement|null>(null);
+    public isDrawing = signal(false)
+    public coords = signal({ x: 0, y: 0 })
 
     ngAfterViewInit(): void {
         if (!this.canvasElement?.nativeElement) return;
@@ -35,6 +37,50 @@ export class GptMessageEditableImageComponent implements AfterViewInit {
         image.onload = () => {
             ctx?.drawImage(image, 0, 0, canvas.width, canvas.height);
         }
+
+    }
+
+    onMouseDown(event: MouseEvent) {
+        if(!this.canvasElement?.nativeElement) return;
+
+        this.isDrawing.set(true);
+
+        const startX = event.clientX - this.canvasElement.nativeElement.getBoundingClientRect().left;
+        const startY = event.clientX - this.canvasElement.nativeElement.getBoundingClientRect().top;
+
+        this.coords.set({ x: startX, y: startY });
+    }
+
+    onMouseMove(event: MouseEvent) {
+        if(!this.isDrawing()) return;
+        if(!this.canvasElement?.nativeElement) return;
+
+        const canvasRef = this.canvasElement.nativeElement;
+
+        const currentX = event.clientX - canvasRef.getBoundingClientRect().left;
+        const currentY = event.clientY - canvasRef.getBoundingClientRect().top;
+
+        const width = currentX - this.coords().x;
+        const height = currentY - this.coords().y;
+
+        const canvasWidth = canvasRef.width;
+        const canvasHeight = canvasRef.height;
+
+        const ctx = canvasRef.getContext('2d')!;
+        ctx.clearRect(0,0, canvasWidth, canvasHeight);
+        ctx.drawImage( this.originalImage()!, 0, 0, canvasWidth, canvasHeight);
+
+        //ctx.fillRect(this.coords().x, this.coords().y, width, height);
+        ctx.clearRect(this.coords().x, this.coords().y, width, height);
+    }
+
+    onMouseUp() {
+        this.isDrawing.set(false);
+
+        const canvas = this.canvasElement!.nativeElement;
+        const url = canvas.toDataURL('image/png');
+
+        this.onSelectedImage.emit(url);
 
     }
 
